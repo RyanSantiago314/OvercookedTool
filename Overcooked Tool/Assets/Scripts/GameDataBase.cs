b0a8b09bff6589096eed82a68778bec258b6c206
Ryan Santiago <ryansantiago.d7s@gmail.com>
2019-10-20 13:10:11 -0700

    Initial Commit: Created values for timers and max number of each order card

---
+++ Overcooked Tool/Assets/Scripts/GameDataBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataBase
+{
+    //values to keep track of timer duration per station
+    public float GameTimer = 600;
+    public float OvenTimer = 25;
+    public float FryerTimer = 15;
+    public float StoveTimer = 20;
+
+    //three of each order card
+    public int CardCount = 3;
+}
