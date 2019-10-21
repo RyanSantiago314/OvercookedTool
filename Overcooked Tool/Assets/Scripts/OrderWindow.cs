c6c48324e8a83762d6f628e592888c8af9fb1f40
Ryan Santiago <ryasanti@ucsc.edu>
2019-10-21 16:58:04 -0700

    Added assets, prototype, and started code

---
+++ Overcooked Tool/Assets/Scripts/OrderWindow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderWindow : MonoBehaviour
+{
+    int[] orderCards = new int[12];
+
+    int[] pointValues = new int[12];
+
+    public enum orderCards
+    {
+        BUR = 0,
+        CHB = 1,
+        CHP = 2,
+        MEP = 3,
+        FIP = 4,
+        FIC = 5,
+        FRI = 6,
+        VEG = 7,
+        GRC = 8,
+        SAL = 9,
+        GAS = 10,
+        SUP = 11
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int = 0; i < orderCards.length; i++)
+        {
+            orderCards[i] = GameDataBase.CardCount;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
